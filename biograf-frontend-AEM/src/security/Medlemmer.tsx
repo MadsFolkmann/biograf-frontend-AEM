076f9174072fdbfd3ce95de60c18befa7f2b3bd9
Abdiox <abdirahmanroble2610@gmail.com>
2024-03-21 18:23:41 +0100

    medlemmer css

--- biograf-frontend-AEM/src/security/Medlemmer.tsx
+++ biograf-frontend-AEM/src/security/Medlemmer.tsx
@@ -14,20 +14,19 @@ interface Medlemmer {
 const Medlemmer: React.FC = () => {
   const [medlemmer, setMedlemmer] = useState<Medlemmer[]>([]);
   const [filteredMedlemmer, setFilteredMedlemmer] = useState<Medlemmer[]>([]);
-  const [filter, setFilter] = useState<string>("all"); // Default filter: vis alle
+  const [filter, setFilter] = useState<string>("all");
 
   useEffect(() => {
     getMedlemmer().then((res) => {
-      console.log("Medlemmer data:", res);
       setMedlemmer(res);
-      setFilteredMedlemmer(res); // Start med at vise alle brugere
+      setFilteredMedlemmer(res);
     });
   }, []);
 
   const handleFilterChange = (newFilter: string) => {
     setFilter(newFilter);
     if (newFilter === "all") {
-      setFilteredMedlemmer(medlemmer); // Vis alle brugere
+      setFilteredMedlemmer(medlemmer);
     } else {
       const filtered = medlemmer.filter((user) => (newFilter === "users" ? user.roleNames.includes("USER") : user.roleNames.includes("ADMIN")));
       setFilteredMedlemmer(filtered);
@@ -44,21 +43,22 @@ const Medlemmer: React.FC = () => {
           <option value="admins">Admins</option>
         </select>
       </div>
-      <ul className="medlemmer-list">
-        {filteredMedlemmer.map((item, index) => (
-          <li key={index} className="medlemmer-item">
-            <div>
-              <span>Brugernavn: {item.username}</span>
-            </div>
-            <div>
-              <span>Email: {item.email}</span>
-            </div>
-            <div>
-              <span>Roller: {item.roleNames.join(", ")}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <table className="medlemmer-table">
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Rolle(r)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredMedlemmer.map((item, index) => (
+            <tr key={index}>
+              <td>{item.email}</td>
+              <td>{item.roleNames.join(", ")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
